2c9cfeb35e99864d3570ed04b01d742052d155fe
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-20 22:53:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 def display_rainbow(colors)
   roygbiv = Array.new
   roygbiv = ["R: ", "O: ", "Y: ", "G: ", "B: ", "V: "]
-  string = roygbiv[0] + colors[0] + roygbiv[1] + colors[1] + roygbiv[2] + colors[2] + roygbiv[3] + colors[3] + roygbiv[4] + colors[4] + roygbiv[5] + colors[5]]
+  string = roygbiv[0] + colors[0] + roygbiv[1] + colors[1] + roygbiv[2] + colors[2] + roygbiv[3] + colors[3] + roygbiv[4] + colors[4] + roygbiv[5] + colors[5]
 
   print stringArray
 end
